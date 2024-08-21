6cb04d8552d2eb5d2506a89589edf4554ac61e76
Mayank Deep <rishusinha525@gmail.com>
2024-08-22 00:45:28 +0530

    changes

---
+++ js/type vs interface/type_vs_interface.js
@@ -0,0 +1,5 @@
+"use strict";
+// type is basically assiginig a data type which we want a variable to store
+let storage;
+// storage = "mk";     // wiil work fine
+// storage = 34;       // will throw errors
---
+++ type vs interface/type_vs_interface.ts
@@ -0,0 +1,13 @@
+
+
+// type is basically assiginig a data type which we want a variable to store
+
+// here stringStorekro is a variable which will store only string data type
+type stringStorekro = string;
+
+
+let storage : stringStorekro;
+
+// storage = "mk";     // wiil work fine
+
+// storage = 34;       // will throw errors
